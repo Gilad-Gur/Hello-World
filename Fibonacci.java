9365215a8294bed55fce19d7ca52704dec9981ab
Gilad-Gur <giladgur0001@gmail.com>
2016-06-19 14:33:10 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
-
+/**
+ * This is a program that represents the Fibonacci sequence. It uses no arrays or String, only a simple loop.
+ * The method gets an integer parameter, to represent the n'th Fibonacci number 
+ * (examples: if n=3 => return 2, because 2 is the 3rd Fibo number. If n=7 => return 13, because 13 is the 7th Fibo number)
+ * 
 public class Fibonacci
 {
     public static long fibo (int n) {
@@ -22,7 +26,7 @@ public class Fibonacci
     }
     public static void main (String[] args) {
         Scanner scan = new Scanner (System.in);
-        System.out.println("Please enter the fibonacci sequance number:");
+        System.out.println("Please enter the fibonacci sequence number:");
         int n = scan.nextInt();
         System.out.println("the value of fibo-n is: " + fibo(n));
     }
